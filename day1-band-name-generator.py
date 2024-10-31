43e757ad8dda41e11d27d730d4d2c5869a141811
Erik <103449678+hrerik@users.noreply.github.com>
2024-10-31 14:37:53 -0300

    Assignment 1 completed

---
+++ day1-band-name-generator.py
@@ -0,0 +1,8 @@
+print("BAND NAME GENERATOR")
+
+city = input("What's the city you grew up in?\n")
+pet_name = input("What's the name of your pet?\n")
+
+band = city + " " + pet_name
+
+print("Your band could be called " + band)
